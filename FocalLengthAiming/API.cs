8c7e03abb2fa36d3fec7f0523b5a77560f14c011
Jesse Ward <jesse@jesseward.net>
2021-12-13 20:12:25 -0800

    Initial commit

---
+++ FocalLengthAiming/API.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace FocalLengthAiming
+{
+    public class API : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            Log.Out(" Loading Patch: " + GetType());
+
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ FocalLengthAiming/PlayerMoveControllerAimPatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace FocalLengthAiming
+{
+
+
+    [HarmonyPatch(typeof(PlayerMoveController))]
+    [HarmonyPatch("Update")]
+    class PlayerMoveControllerAimPatch
+    {
+
+        private static float Radians(float degrees)
+        {
+            return ((float)Math.PI / 180) * degrees;
+        }
+
+        public static bool Prefix(PlayerMoveController __instance, 
+            EntityPlayerLocal ___entityPlayerLocal, 
+            float ___defaultSensitivity, 
+            ref float ___aimingSensitivity)
+
+
+        {
+            if (!___entityPlayerLocal.AimingGun)
+            {
+                ___aimingSensitivity = ___defaultSensitivity;
+                return true;
+            }
+
+            float defaultFOV = GamePrefs.GetInt(EnumGamePrefs.OptionsGfxFOV);
+            var aimFOV = ___entityPlayerLocal.cameraTransform.GetComponent<Camera>().fieldOfView;
+
+            ___aimingSensitivity = (float)(Math.Tan(Radians(aimFOV / 2)) / Math.Tan(Radians(defaultFOV / 2)));
+
+            return true;
+        }
+    }
+}
