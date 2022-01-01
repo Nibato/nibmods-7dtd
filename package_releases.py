cec28fab0d132e70dc7bab45122417fde34865ba
Jesse Ward <jesse@jesseward.net>
2022-01-01 06:04:39 -0800

    Fixed linter warnings

--- package_releases.py
+++ package_releases.py
@@ -5,10 +5,12 @@ import os
 from dataclasses import dataclass
 import shutil
 
+
 MODS_PATH = r'C:\Program Files (x86)\Steam\steamapps\common\7 Days To Die\Mods'
 EXPECTED_AUTHOR = 'nibato'
 
 RELEASE_DIR = os.path.join(os.getcwd(), "PackagedReleases")
+
 
 @dataclass
 class ModInfo:
@@ -17,7 +19,8 @@ class ModInfo:
     author: str
     version: str
 
-def getModInfo(path: str):
+
+def get_mod_info(path: str):
     info_path = os.path.join(path,'ModInfo.xml')
 
     if not os.path.isfile(info_path) or os.path.isfile(os.path.join(path, 'skip')):
@@ -29,15 +32,15 @@ def getModInfo(path: str):
         print("Error parsing '{}': {}".format(path, e))
         return None
 
-    authorElement = root.find('.//ModInfo/Author[@value]')
-    versionElement = root.find('.//ModInfo/Version[@value]')
+    author_element = root.find('.//ModInfo/Author[@value]')
+    version_element = root.find('.//ModInfo/Version[@value]')
 
-    if authorElement is None or versionElement is None:
+    if author_element is None or version_element is None:
         return None
-    
+
     name = os.path.basename(path)
-    author = authorElement.attrib['value']
-    version = versionElement.attrib['value']
+    author = author_element.attrib['value']
+    version = version_element.attrib['value']
 
     if author.casefold() != EXPECTED_AUTHOR.casefold():
         return None
@@ -45,7 +48,7 @@ def getModInfo(path: str):
     return ModInfo(name, path, author, version)
 
 
-def packageMod(mod: ModInfo):
+def package_mod(mod: ModInfo):
     archive_name = "{}_v{}".format(mod.name, mod.version)
     archive_path = os.path.join(RELEASE_DIR, archive_name)
 
@@ -53,12 +56,12 @@ def packageMod(mod: ModInfo):
 
     print("Creating package '{}'".format(archive_name))
 
-    shutil.make_archive(archive_path, 
+    shutil.make_archive(archive_path,
         format='zip',
         root_dir=MODS_PATH,
         base_dir=base_path
         )
-    pass
+
 
 def main():
     mods = []
@@ -69,25 +72,25 @@ def main():
     for d in os.listdir(MODS_PATH):
         path = os.path.join(MODS_PATH,d)
 
-        modInfo = getModInfo(path)
+        mod_info = get_mod_info(path)
 
-        if modInfo is None:
+        if mod_info is None:
             continue
 
-        mods.append(modInfo)
+        mods.append(mod_info)
 
     if len(mods) <= 0:
         print("No eligible mods found")
         return
-    
+
     if os.path.isdir(RELEASE_DIR):
         shutil.rmtree(RELEASE_DIR)
 
     os.makedirs(RELEASE_DIR)
 
     for mod in mods:
-        packageMod(mod)
-    
+        package_mod(mod)
+
 
 if __name__ == "__main__":
     main()
