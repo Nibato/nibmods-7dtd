9b6d2b6a664c1a48eff336e9bdf74a80894f22bb
Jesse Ward <jesse@jesseward.net>
2022-01-08 21:22:47 -0800

    Removed redundant classes for IModAPI implementations

--- AutoAcceptPartyInvite/API.cs
+++ AutoAcceptPartyInvite/API.cs
@@ -2,17 +2,14 @@
 
 namespace AutoAcceptPartyInvite
 {
-    public class AutoAcceptPartyInvite
+    public class API : IModApi
     {
-        public class API : IModApi
+        public void InitMod(Mod _modInstance)
         {
-            public void InitMod(Mod _modInstance)
-            {
-                Log.Out(" Loading Patch: " + GetType());
+            Log.Out(" Loading Patch: " + GetType());
 
-                var harmony = new HarmonyLib.Harmony(GetType().ToString());
-                harmony.PatchAll(Assembly.GetExecutingAssembly());
-            }
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
 }
--- UnlockedByDefault/API.cs
+++ UnlockedByDefault/API.cs
@@ -2,17 +2,14 @@
 
 namespace UnlockedByDefault
 {
-    public class UnlockedByDefault
+    public class API : IModApi
     {
-        public class API : IModApi
+        public void InitMod(Mod _modInstance)
         {
-            public void InitMod(Mod _modInstance)
-            {
-                Log.Out(" Loading Patch: " + GetType());
+            Log.Out(" Loading Patch: " + GetType());
 
-                var harmony = new HarmonyLib.Harmony(GetType().ToString());
-                harmony.PatchAll(Assembly.GetExecutingAssembly());
-            }
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
 }
