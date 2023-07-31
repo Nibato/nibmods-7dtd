115a5436a30d4f0377c4894f93cc828a59a584ec
Jesse Ward <jesse@jesseward.net>
2023-07-31 06:18:24 -0700

    Preliminary A21 update to FocalLengthAiming

--- FocalLengthAiming/PlayerMoveControllerAimPatch.cs
+++ FocalLengthAiming/PlayerMoveControllerAimPatch.cs
@@ -1,38 +1,51 @@
-﻿using System;
+﻿using HarmonyLib;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Reflection;
-using HarmonyLib;
+using System.Reflection.Emit;
 using UnityEngine;
 
 namespace FocalLengthAiming
 {
     [HarmonyPatch(typeof(PlayerMoveController))]
     [HarmonyPatch("Update")]
-    class PlayerMoveControllerAimPatch
+    internal class PlayerMoveControllerAimPatch
     {
-        public static bool Prefix(PlayerMoveController __instance, 
-            EntityPlayerLocal ___entityPlayerLocal, 
-            float ___defaultSensitivity, 
-            ref float ___aimingSensitivity)
-
+        public static bool Prefix(PlayerMoveController __instance,
+            EntityPlayerLocal ___entityPlayerLocal,
+            float ___lookSensitivity,
+            ref float ___zoomSensitivity)
 
         {
             if (!___entityPlayerLocal.AimingGun)
             {
-                ___aimingSensitivity = ___defaultSensitivity;
+                ___zoomSensitivity = ___lookSensitivity;
                 return true;
             }
 
             float defaultFOV = GamePrefs.GetInt(EnumGamePrefs.OptionsGfxFOV);
             var aimFOV = ___entityPlayerLocal.cameraTransform.GetComponent<Camera>().fieldOfView;
 
-            var sensMult = (Mathf.Tan((aimFOV / 2) * Mathf.Deg2Rad) / Mathf.Tan((defaultFOV / 2) * Mathf.Deg2Rad));
-            ___aimingSensitivity = ___defaultSensitivity * sensMult;
+            var sensMult = Mathf.Tan(aimFOV / 2 * Mathf.Deg2Rad) / Mathf.Tan(defaultFOV / 2 * Mathf.Deg2Rad);
+            ___zoomSensitivity = ___lookSensitivity * sensMult;
 
             return true;
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var lookSensitivityRef = AccessTools.Field(typeof(PlayerMoveController), "lookSensitivity");
+            var zoomSensitivityRef = AccessTools.Field(typeof(PlayerMoveController), "zoomSensitivity");
+
+            var c = new CodeMatcher(instructions)
+                .MatchForward(false,
+                              new CodeMatch((i) => i.opcode == OpCodes.Ldloc_S),
+                              new CodeMatch(OpCodes.Ldarg_0),
+                              new CodeMatch(OpCodes.Ldfld, lookSensitivityRef),
+                              new CodeMatch((i) => i.opcode == OpCodes.Ble_Un));
+            c.RemoveInstructions(4);
+            c.Advance(1);
+
+            c.Operand = zoomSensitivityRef;
+            return c.InstructionEnumeration();
         }
     }
 }
